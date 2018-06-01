8f853704b29836db3896beef5304c5889b536c7c
Bartek <brt0555@interia.eu>
2018-06-01 15:52:46 +0200

    Init commit setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { FooterComponent } from './footer/footer.component';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
+import { HomeCarouselComponent } from './home/home-carousel/home-carousel.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    HomeCarouselComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    SharedModule
+  ],
+  exports: [
+    AppRoutingModule,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
+  ],
+})
+export class CoreModule {}
